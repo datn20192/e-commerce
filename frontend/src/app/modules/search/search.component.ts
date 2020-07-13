3255631bf4de2064645bfb066a2e6e185521ab41
NVD <ngovietdung0112@gmail.com>
2020-07-13 22:52:33 +0700

    fix search component

--- frontend/src/app/modules/search/search.component.ts
+++ frontend/src/app/modules/search/search.component.ts
@@ -17,7 +17,7 @@ export class SearchComponent implements OnInit {
   private pages: number[];
   private totalElement: number;
   private keyWordString: string;
-  private modal:boolean;
+  private modal: boolean;
   heading3: string;
 
   constructor(
@@ -36,19 +36,20 @@ export class SearchComponent implements OnInit {
   load() {
     this.authService.user$.subscribe(user => {
       this.modal = (user.roles.admin) ? false : true;
-      this.productsListSubs = this.share.currentSearchedString.subscribe(str => {
-        this.productApiService.getProductByFilter(str, this.page, 12).subscribe(res => {
-          this.keyWordString = str;
-          let result = JSON.parse(res);
-          this.productsList = result.data['content'];
-          this.pages = new Array(result.data['totalPages']);
-          this.totalElement = result.data['totalElement'];
-          this.heading3 = `Kết quả tìm kiếm cho '${this.keyWordString}': ${this.totalElement} kết quả`;
-        },
-          console.error
-        );
-      })
     });
+    this.productsListSubs = this.share.currentSearchedString.subscribe(str => {
+      this.productApiService.getProductByFilter(str, this.page, 12).subscribe(res => {
+        this.keyWordString = str;
+        let result = JSON.parse(res);
+        this.productsList = result.data['content'];
+        this.pages = new Array(result.data['totalPages']);
+        this.totalElement = result.data['totalElement'];
+        this.heading3 = `Kết quả tìm kiếm cho '${this.keyWordString}': ${this.totalElement} kết quả`;
+      },
+        console.error
+      );
+    })
+
   }
   showCurrentPage(currentPage) {
     this.page = currentPage;
